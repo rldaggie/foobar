173a4665c69164519c1152f16598fba264b456a5
Riley Dallas <rldaggie@gmail.com>
2016-03-01 21:25:47 -0600

    Added api method

--- Gemfile
+++ Gemfile
@@ -2,5 +2,6 @@ source 'https://rubygems.org'
 
 ruby '2.2.3'
 
+gem 'httparty'
 gem 'sinatra'
 gem 'sinatra-contrib'
--- app.rb
+++ app.rb
@@ -1,9 +1,18 @@
+require 'httparty'
 require 'sinatra'
 require 'sinatra/json'
 require 'json'
 
 get '/' do
   erb :index, layout: true
+end
+
+get '/search' do
+  results = []
+  if params[:q] && params[:q].length > 0
+    results << get_results(params[:q])
+  end
+  json results
 end
 
 get '/favorites' do
@@ -21,4 +30,14 @@ post '/favorites' do
   file << movie
   File.write('data.json',JSON.pretty_generate(file))
   movie.to_json
+end
+
+def get_results(query)
+  begin
+    response = HTTParty.get("https://www.omdbapi.com?t=#{query}").parsed_response
+    puts "response: #{response}"
+    response
+  rescue
+    []
+  end
 end
